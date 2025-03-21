c76343ca5894c8cfa70b433a5dc905f65d7b5a87
hushao1212 <131630455+hushao1212@users.noreply.github.com>
2025-03-21 17:03:16 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -24,3 +24,29 @@ const hookClick = (e) => {
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+// 定义刷新间隔时间，这里设置为 5000 毫秒（即 5 秒）
+const refreshInterval = 60000;
+
+// 使用 setTimeout 函数来设置定时刷新
+setTimeout(() => {
+    // 刷新当前页面
+    location.reload();
+}, refreshInterval);
+// 用于存储已经打开的窗口
+const openedWindows = {};
+
+function openUniqueWindow(url, windowName) {
+    // 检查该窗口是否已经打开
+    if (openedWindows[windowName] &&!openedWindows[windowName].closed) {
+        // 如果已经打开且未关闭，则将其聚焦
+        openedWindows[windowName].focus();
+    } else {
+        // 打开新窗口
+        const newWindow = window.open(url, windowName);
+        if (newWindow) {
+            // 存储新打开的窗口
+            openedWindows[windowName] = newWindow;
+        }
+    }
+}
+
